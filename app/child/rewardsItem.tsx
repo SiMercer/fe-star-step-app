6b1a3d1bd0c7f8848726a8e7462d1ca205d48f93
e-hoog <eddiehoog@hotmail.com>
2025-04-16 16:24:57 +0100

    create slider button for child to request reward

--- app/child/rewardsItem.tsx
+++ app/child/rewardsItem.tsx
@@ -16,6 +16,46 @@ export default function ChildsRewardsItem() {
       }}
     >
       <Text>Rewards text</Text>
+      <View style={{ alignItems: "flex-end" }}>
+        <Pressable
+          style={{
+            height: 24,
+            width: 45,
+            borderRadius: 12,
+            borderWidth: 2,
+            borderColor: "#000",
+            justifyContent: "center",
+          }}
+          onPress={() => {
+            setIsRequested(!isRequested);
+          }}
+        >
+          {isRequested ? (
+            <View
+              style={{
+                height: 12,
+                width: 12,
+                borderRadius: 6,
+                backgroundColor: "#000",
+                alignSelf: "flex-end",
+                margin: 5,
+              }}
+            ></View>
+          ) : (
+            <View
+              style={{
+                height: 12,
+                width: 12,
+                borderRadius: 6,
+                backgroundColor: "#000",
+                alignSelf: "flex-start",
+                margin: 5,
+              }}
+            ></View>
+          )}
+        </Pressable>
+        <View>{isRequested ? "Requested!" : "Request?"}</View>
+      </View>
     </View>
   );
 }

0c5e5d3a2dc7872fd4d4ce350cb41717667cfc81
SiMercer <na>
2025-04-17 12:11:12 +0100

    consolelog added for auth0 debugging

--- authConfig.ts
+++ authConfig.ts
@@ -2,10 +2,15 @@ import Constants from "expo-constants";
 
 const { auth0Domain, auth0ClientId, auth0Audience, redirectUri } = Constants.expoConfig.extra;
 
+
+console.log("Loaded Auth0 domain:", authConfig.domain);
+console.log("Audience:", authConfig.audience);
+console.log("Redirect URI:", authConfig.redirectUri);
+
 export const authConfig = {
-  domain: auth0Domain,
-  clientId: auth0ClientId,
-  audience: auth0Audience,
-  redirectUri,
-  useProxy: process.env.NODE_ENV !== "production",
+  domain: process.env.EXPO_PUBLIC_AUTH0_DOMAIN!,
+  clientId: process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID!,
+  audience: process.env.EXPO_PUBLIC_AUTH0_AUDIENCE!,
+  redirectUri: process.env.EXPO_PUBLIC_REDIRECT_URI!,
+  useProxy: false,
 };

187f90e440c223443f5a5360f030f0d03f6fc7cc
SiMercer <na>
2025-04-17 12:40:49 +0100

    consolelog added for auth0 debugging

--- authConfig.ts
+++ authConfig.ts
@@ -1,18 +1,21 @@
 import Constants from "expo-constants";
 
-const { auth0Domain, auth0ClientId, auth0Audience, redirectUri } = Constants.expoConfig.extra;
+const {
+  auth0Domain,
+  auth0ClientId,
+  auth0Audience,
+  redirectUri
+} = Constants.expoConfig.extra;
 
-
-console.log("Loaded Auth0 domain:", authConfig.domain);
-console.log("Audience:", authConfig.audience);
-console.log("Redirect URI:", authConfig.redirectUri);
-console.log("Auth0 domain loaded from env:", Constants.expoConfig.extra?.auth0Domain);
+console.log("Loaded Auth0 domain:", auth0Domain);
+console.log("Audience:", auth0Audience);
+console.log("Redirect URI:", redirectUri);
 
 export const authConfig = {
-  domain: import.meta.env.VITE_AUTH0_DOMAIN || process.env.EXPO_PUBLIC_AUTH0_DOMAIN,
-  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID || process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID,
-  audience: import.meta.env.VITE_AUTH0_AUDIENCE || process.env.EXPO_PUBLIC_AUTH0_AUDIENCE,
-  redirectUri: import.meta.env.VITE_REDIRECT_URI || process.env.EXPO_PUBLIC_REDIRECT_URI,
+  domain: auth0Domain,
+  clientId: auth0ClientId,
+  audience: auth0Audience,
+  redirectUri: redirectUri,
   useProxy: false,
   scopes: ["openid", "profile", "email"],
   responseType: "token",

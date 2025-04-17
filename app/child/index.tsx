b7b33523014f9515301eeb1fbebd11bb270c43b1
kozadireza <hanna.zhamoida1993@gmail.com>
2025-04-17 11:26:06 +0100

    Kid dash board in progress

--- app/child/index.tsx
+++ app/child/index.tsx
@@ -1,25 +1,54 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image } from "react-native";
 import { useRouter, Link } from "expo-router";
-
 
 export default function ChildDashboardScreen() {
   const router = useRouter();
 
-return (
+  return (
     <View style={{ flex: 1, justifyContent: "space-between", padding: 20 }}>
-        
-      <Text style={{ fontSize: 24, marginTop: 20 }}>Child Dashboard</Text>
+      <View
+        className="headerForKidDashboard"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: "51px",
+        }}
+      >
+        <View
+          style={{ display: "flex", width: "30%", alignItems: "flex-start" }}
+        >
+          <Image
+            source={require("../../assets/images/react-logo.png")}
+            style={{ width: 100, height: 100 }}
+            resizeMode="contain"
+          />
+        </View>
+        <Text style={{ fontSize: 24, marginTop: 20, width: "70%" }}>
+          Child Dashboard
+        </Text>
+      </View>
 
-      <View style={{ flexDirection: "row", justifyContent: "space-around", marginBottom: 20 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          marginBottom: 20,
+        }}
+      >
         <Link href="/" asChild>
           <Button title="Back" onPress={() => {}} />
         </Link>
       </View>
-      
 
-
-      <View style={{ flexDirection: "row", justifyContent: "space-around", marginBottom: 20 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          marginBottom: 20,
+        }}
+      >
         <Link href="/child" asChild>
           <Button title="Dashboard" onPress={() => {}} />
         </Link>

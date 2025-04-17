ff4853403fc8511c70f31bdfa3fe800bc7e4c106
SiMercer <na>
2025-04-17 10:48:13 +0100

    consolelog added for auth0 debugging

--- authConfig.js
+++ authConfig.js
@@ -6,11 +6,9 @@ const useProxy = false;
 console.log("Loaded Auth0 domain:", Constants.expoConfig.extra.auth0Domain);
 
 export const authConfig = {
-  domain: Constants.expoConfig.extra.EXPO_PUBLIC_AUTH0_DOMAIN,
-  clientId: Constants.expoConfig.extra.EXPO_PUBLIC_AUTH0_CLIENT_ID,
-  audience: Constants.expoConfig.extra.EXPO_PUBLIC_AUTH0_AUDIENCE,
-  redirectUri: Constants.expoConfig.extra.EXPO_PUBLIC_REDIRECT_URI,
-  useProxy,
-  scopes: ["openid", "profile", "email"],
-  responseType: "code",
+  domain: process.env.EXPO_PUBLIC_AUTH0_DOMAIN,
+  clientId: process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID,
+  audience: process.env.EXPO_PUBLIC_AUTH0_AUDIENCE,
+  redirectUri: process.env.EXPO_PUBLIC_REDIRECT_URI,
+  useProxy: false,
 };

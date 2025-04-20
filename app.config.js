beccf43fa086bf40c4a0a9ca1debe0c8bec55e70
SiMercer <na>
2025-04-21 00:23:34 +0100

    changed Onrender settings

--- app.config.js
+++ app.config.js
@@ -1,5 +1,3 @@
-import "dotenv/config";
-
 export default {
   expo: {
     name: "StarSteps",
@@ -11,7 +9,7 @@ export default {
       auth0Domain: process.env.EXPO_PUBLIC_AUTH0_DOMAIN,
       auth0ClientId: process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID,
       auth0Audience: process.env.EXPO_PUBLIC_AUTH0_AUDIENCE,
-    },
+      redirectUri: process.env.EXPO_PUBLIC_REDIRECT_URI,
     web: {
       bundler: "metro",
     },

111b822b91ca15ce074db36c60d1b2a908cdf52e
SiMercer <na>
2025-04-21 19:55:36 +0100

    Auth0 setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,6 @@
+import * as WebBrowser from 'expo-web-browser';
+WebBrowser.maybeCompleteAuthSession();
+
 import React from "react";
 import { View, Text, Button, ScrollView } from "react-native";
 import { Link } from "expo-router";

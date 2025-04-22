cb6fb4c6e11aae6315ea11aa0211a01bf69878b8
kozadireza <hanna.zhamoida1993@gmail.com>
2025-04-22 22:09:32 +0100

    Kid dashboard functionality was finished

--- app/child/index.tsx
+++ app/child/index.tsx
@@ -1,39 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, Button, Image } from "react-native";
 import { useRouter, Link } from "expo-router";
-import {
-  getKidsByParent,
-  getRewardsByParent,
-  getTasksByKid,
-} from "@/utils/api";
+import { getRewardsByParent, getTasksByParent } from "@/utils/api";
 import NavBarKid from "./NavBarKid";
+import { useChild } from "@/contexts/ChildContext";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function ChildDashboardScreen() {
-  const [kid, setKid] = useState({});
+  const { selectedChild } = useChild();
+  const { parent } = useAuth();
+
+  console.log(selectedChild, "<<<<<child from cont");
+  console.log(parent, "<<<<<parent from cont");
+  const [kid, setKid] = useState<object>({});
   const [tasks, setTasks] = useState([]);
   const [rewards, setRewards] = useState([]);
   // const { kid } = useKid();
   const router = useRouter();
 
-  const child_id = "000000000000000000000002";
-  const parent_id = "auth0|test123";
   useEffect(() => {
-    async function kidData() {
-      const kid = await getKidsByParent(parent_id);
-      setKid(kid[0]);
-    }
-    kidData();
+    setKid(selectedChild);
 
     async function getKidRewards() {
-      const rewardsData = await getRewardsByParent(parent_id);
+      const rewardsData = await getRewardsByParent(parent._id);
       setRewards(rewardsData.rewards);
     }
     getKidRewards();
   }, []);
+
   useEffect(() => {
     async function getKidTasks() {
-      const tasks = await getTasksByKid(kid._id);
-      setTasks(tasks);
+      const tasks = await getTasksByParent(parent._id);
+      const kidTask = tasks.filter((task: any) => {
+        return task.assignedTo === kid._id;
+      });
+
+      setTasks(kidTask);
     }
     getKidTasks();
   }, [kid]);
@@ -145,9 +147,9 @@ export default function ChildDashboardScreen() {
             }}
           >
             <Text>
-              {/* {tasks.length <= 1
+              {tasks.length <= 1
                 ? `${tasks.length} task for today`
-                : `${tasks.length} tasks for today`} */}
+                : `${tasks.length} tasks for today`}
             </Text>
           </View>
         </Link>
